7874abdee926c1849af17137f09942b777d80b3a
AlexOrsak <alexorsak@gmail.com>
2026-05-25 18:37:09 -0500

    Coroutines and bug fixes

--- .tts/objects/Bag.24f6d0.lua
+++ .tts/objects/Bag.24f6d0.lua
@@ -2,6 +2,8 @@ local NOTE_NAME = "ArmyList"
 local Utils = require("utils")
 local modelScripts = require("modelScripts")
 
+local SPAWN_BATCH_SIZE = 1
+
 function onLoad()
 	self.createButton({
 		label = "Spawn Army",
@@ -28,13 +30,14 @@ end
 
 local psu = {}
 local contents = nil
+local spawnQueue = {}
+
 function processList(text, color)
-    
 	contents = self.getData().ContainedObjects
     local basePos = {
-		x = self.getPosition().x,
-		y = self.getPosition().y + 2,
-		z = self.getPosition().z + 4
+		x = 70,
+		y = 1,
+		z = -17
 	}
 	local rowSpacing, colSpacing, warbandsPerRow = 10, 8, 6
 	local modelIndex, warbandIndex = 0, 0
@@ -45,45 +48,68 @@ function processList(text, color)
         y = basePos.y,
         z = basePos.z
     }
+    local warbandBase = {x = basePos.x, z = basePos.z}
     local unit_pos = {
-        x = heroPos.x,
-        y = heroPos.y,
-        z = heroPos.z + 1.5
+        x = basePos.x,
+        y = basePos.y,
+        z = basePos.z + 1.5
     }
     local floor = math.floor
     local hasHero = false
 
-	for line in text:gmatch("%(%s*(.-)%s*%)") do --TODO fix gmatch bug here 
+	spawnQueue = {}
+	for line in text:gmatch("%(%s*(.-)%s*%)") do --TODO fix gmatch bug here
         local count, name, wargear = line:match("^(%d*)x*%s*(.+:)%s*( *[%S ]*)%s*") --TODO check for match bug here
         if name and count ~= "0" then
+            local fullName = wargear ~= "" and name .. " " .. wargear or name
             if count ~= "" then
                 if not hasHero then
                     broadcastToColor("Error: \"" .. name .. "\" found before any hero.", color, {1, 0, 0})
                 else
-                    spawnNextFromQueue(name .. wargear, unit_pos, color)
-                    totalModels = totalModels + 1
-                    modelIndex = modelIndex + 1
-                    unit_pos.x = heroPos.x + (modelIndex % 6) * 1.1
-                    unit_pos.z = heroPos.z + floor(modelIndex / 6) * 1.1
+                    for i = 1, tonumber(count) do
+                        spawnQueue[#spawnQueue + 1] = {name = fullName, pos = {x = unit_pos.x, y = unit_pos.y, z = unit_pos.z}, color = color}
+                        totalModels = totalModels + 1
+                        modelIndex = modelIndex + 1
+                        unit_pos.x = warbandBase.x + (modelIndex % 6) * 1.1
+                        unit_pos.z = warbandBase.z + floor(modelIndex / 6) * 1.1
+                    end
                 end
             else
                 hasHero = true
-                spawnNextFromQueue(name .. wargear, heroPos, color)
+                spawnQueue[#spawnQueue + 1] = {name = fullName, pos = {x = heroPos.x, y = heroPos.y, z = heroPos.z}, color = color}
+                warbandBase.x = heroPos.x
+                warbandBase.z = heroPos.z + 1.5
                 warbandIndex = warbandIndex + 1
                 row = floor(warbandIndex / warbandsPerRow)
                 col = warbandIndex % warbandsPerRow
                 heroPos.x = basePos.x + row * rowSpacing
                 heroPos.z = basePos.z + col * colSpacing
-                unit_pos.x = heroPos.x
-                unit_pos.z = heroPos.z + 1.5
+                unit_pos.x = warbandBase.x
+                unit_pos.z = warbandBase.z
                 totalModels = totalModels + 1
                 modelIndex = 0
             end
         end
 	end
 	broadcastToColor(totalModels .. " Models | " .. floor(totalModels / 2) + 1 .. " Break | " .. floor(totalModels / 4) .. " Remaining", color, {0.2, 1, 0.2})
+    startLuaCoroutine(self, "spawnCoroutine")
+end
+
+function spawnCoroutine()
+    local i = 1
+    while i <= #spawnQueue do
+        for b = 1, SPAWN_BATCH_SIZE do
+            if i > #spawnQueue then break end
+            local task = spawnQueue[i]
+            spawnNextFromQueue(task.name, task.pos, task.color)
+            i = i + 1
+        end
+        coroutine.yield(0)
+    end
+    spawnQueue = {}
     contents = nil
     psu = {}
+    return 1
 end
 
 function spawnNextFromQueue(name, pos, color)
@@ -95,18 +121,29 @@ function spawnNextFromQueue(name, pos, color)
     local colonIdx = nameLower:find(":")
     local baseName = colonIdx and nameLower:sub(1, colonIdx) or nameLower
 
-    local template = psu[baseName]
+    local template = psu[nameLower] or psu[baseName]
     if not template then
+        local baseMatch = nil
         for _, obj in ipairs(contents) do
             local objLower = obj["Nickname"]:lower()
-            local objColon = objLower:find(":")
-            local objBase = objColon and objLower:sub(1, objColon) or objLower
-            if objBase == baseName then
+            if objLower == nameLower then
                 obj.LuaScript = modelScripts.getScript(obj.Tags)
-                psu[baseName] = obj
+                psu[nameLower] = obj
                 template = obj
                 break
             end
+            if not baseMatch then
+                local objColon = objLower:find(":")
+                local objBase = objColon and objLower:sub(1, objColon) or objLower
+                if objBase == baseName then
+                    baseMatch = obj
+                end
+            end
+        end
+        if not template and baseMatch then
+            baseMatch.LuaScript = modelScripts.getScript(baseMatch.Tags)
+            psu[baseName] = baseMatch
+            template = baseMatch
         end
     end
 
